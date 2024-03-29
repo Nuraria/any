cf3c4029da47c8971410416435558454814ed181
MagneticShade <senya.krivorot11@gmail.com>
2024-03-29 15:31:59 +0200

    wip4

--- app/main.py
+++ app/main.py
@@ -2,10 +2,18 @@ from fastapi import FastAPI
 from app.routers import category_router,collection_router,user_router,product_router
 from app.models import Base
 from app.database import engine
-Base.metadata.drop_all(bind=engine)
+from fastapi.middleware.cors import CORSMiddleware
 Base.metadata.create_all(bind=engine)
 
+
 app=FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(router=category_router)
 app.include_router(router=collection_router)
