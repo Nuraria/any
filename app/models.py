7d5e0aae1277daf4f96b0db1ae971f5917eb1c14
MagneticShade <senya.krivorot11@gmail.com>
2024-03-29 16:07:01 +0200

    wip5

--- app/models.py
+++ app/models.py
@@ -1,6 +1,5 @@
 from app.database import Base
 from sqlalchemy import Column,Integer,String,ForeignKey
-
 class User(Base):
     __tablename__="user"
     id = Column(Integer,primary_key=True)
--- app/schemas/Category.py
+++ app/schemas/Category.py
@@ -1,13 +1,14 @@
 from pydantic import BaseModel
+from pydantic_extra_types.color import Color
 from typing import Optional
 
 class CategoryCreate(BaseModel):
     category_name:str
-    color:str
+    color:Color
 
 class Category(CategoryCreate):
     id:int
 
 class CategoryUpdate(BaseModel):
     category_name:Optional[str]=None
-    color:Optional[str]=None
+    color:Optional[Color]=None
